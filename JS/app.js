8c49a9f007a07903afedfa50494b5727a2baf9f7
KarlaMartinez17 <karla.martinez_@hotmail.com>
2018-04-11 12:29:30 -0500

    corrigiendo indentacion

--- JS/app.js
+++ JS/app.js
@@ -7,145 +7,96 @@ function cipher(){
     alert('Por favor ingresa una palabra para encriptar');
     return;
   }// cierre de If
-/*
-vamos a convertir el valor ingresado por el usuario a mayúsculas
-para no tener problema con la fórmula de cifrado cesar. Ya que en el codigo ASCII
-el abecedario en mayúsculas se encuentra del número 65 al 90
-y la fórmula enseñada en el LMS trabaja con el 65
-*/
-msg = msg.toUpperCase();
+  /*
+  vamos a convertir el valor ingresado por el usuario a mayúsculas
+  para no tener problema con la fórmula de cifrado cesar. Ya que en el codigo ASCII
+  el abecedario en mayúsculas se encuentra del número 65 al 90
+  y la fórmula enseñada en el LMS trabaja con el 65
+  */
+  msg = msg.toUpperCase();
 
-//creando una variable que almacene el valor otorgado por el usuario cuando haya pasado las validaciones.
-var finalMsg = '';
+  //creando una variable que almacene el valor otorgado por el usuario cuando haya pasado las validaciones.
+  var finalMsg = '';
 
-//Iterar el string otorgado por el usuario por medio de un ciclo for.
-for(var i = 0; i < msg.length; i++) {
-  var iLetter = msg[i];
-// verificar si el valor tiene numeros.
-  if(iLetter === '0' ||
-     iLetter === '1' ||
-     iLetter === '2' ||
-     iLetter === '3' ||
-     iLetter === '4' ||
-     iLetter === '5' ||
-     iLetter === '6' ||
-     iLetter === '7' ||
-     iLetter === '8' ||
-     iLetter === '9'){
+  //Iterar el string otorgado por el usuario por medio de un ciclo for.
+  for(var i = 0; i < msg.length; i++) {
+    var iLetter = msg[i];
+  // verificar si el valor tiene numeros.
+      if(iLetter === '0' ||
+        iLetter === '1' ||
+        iLetter === '2' ||
+        iLetter === '3' ||
+        iLetter === '4' ||
+        iLetter === '5' ||
+        iLetter === '6' ||
+        iLetter === '7' ||
+        iLetter === '8' ||
+        iLetter === '9'){
 
-       alert('La palabra ingresada contiene números, favor de ingresar una palabra para encriptar');
-  return;
+          alert('La palabra ingresada contiene números, favor de ingresar una palabra para encriptar');
+            return;
      }//cierre de if numeros
-
-//creando una variable que almacene el codigo ASCII de la variable msg.
-var asciiCharCode = msg.charCodeAt(i);
-//creando variable que almacene los espacios a recorrer.
-var totalOfSpaces = 33;
-//creando variable con la fórmula del codigo César.
-   var newCharCode = ((asciiCharCode - 65 + totalOfSpaces) % 26) + 65;
-   finalMsg = finalMsg + String.fromCharCode(newCharCode);
-
-   }//cierre de for
-
-  return finalMsg;
-
+  //creando una variable que almacene el codigo ASCII de la variable msg.
+  var asciiCharCode = msg.charCodeAt(i);
+  //creando variable que almacene los espacios a recorrer.
+  var totalOfSpaces = 33;
+  //creando variable con la fórmula del codigo César.
+  var newCharCode = ((asciiCharCode - 65 + totalOfSpaces) % 26) + 65;
+    finalMsg = finalMsg + String.fromCharCode(newCharCode);
+  }//cierre de for
+    return finalMsg;
 }// cierre de función
 
 // Creando función decipher.
 function decipher(){
 
   // creando una variable que guarde el valor ingresado por el usuario
-var msg = prompt('Por favor ingresa una palabra para encriptar.');
-//creando primera función para cifrar el valor ingresado por el usuario
+  var msg = prompt('Por favor ingresa una palabra para encriptar.');
+  //creando primera función para cifrar el valor ingresado por el usuario
 function cipher(){
   //revisaremos que el valor que de el usuario NO se encuentre vacio.
-  if(msg.length === 0){
-    alert('Por favor ingresa una palabra para encriptar');
-    return;
-  }// cierre de If
-/*
-vamos a convertir el valor ingresado por el usuario a mayúsculas
-para no tener problema con la fórmula de cifrado cesar. Ya que en el codigo ASCII
-el abecedario en mayúsculas se encuentra del número 65 al 90
-y la fórmula enseñada en el LMS trabaja con el 65
-*/
+      if(msg.length === 0){
+        alert('Por favor ingresa una palabra para encriptar');
+        return;
+    }// cierre de If
+
 msg = msg.toUpperCase();
 
-//creando una variable que almacene el valor otorgado por el usuario cuando haya pasado las validaciones.
-var finalMsg = '';
-
-//Iterar el string otorgado por el usuario por medio de un ciclo for.
-for(var i = 0; i < msg.length; i++){
-  var iLetter = msg[i];
-// verificar si el valor tiene numeros.
-  if(iLetter === '0' ||
-     iLetter === '1' ||
-     iLetter === '2' ||
-     iLetter === '3' ||
-     iLetter === '4' ||
-     iLetter === '5' ||
-     iLetter === '6' ||
-     iLetter === '7' ||
-     iLetter === '8' ||
-     iLetter === '9'){
-
-       alert('La palabra ingresada contiene números, favor de ingresar una palabra para encriptar');
-  return;
-     }//cierre de if numeros
-
-//creando una variable que almacene el codigo ASCII de la variable msg.
-var asciiCharCode = msg.charCodeAt(i);
-//creando variable que almacene los espacios a recorrer.
-var totalOfSpaces = 33;
-//creando variable con la fórmula del codigo César.
-   var newCharCode = ((asciiCharCode - 65 + totalOfSpaces) % 26) + 65;
-   finalMsg = finalMsg + String.fromCharCode(newCharCode);
-
-   }//cierre de for
-  return finalMsg;
-
-}// cierre de función
-
-// Creando función decipher.
-function decipher(){
-
-  if(msg.length === 0){
-    alert('Por favor ingresa una palabra para encriptar');
-    return;
-  }
-  msg = msg.toUpperCase();
+  //creando una variable que almacene el valor otorgado por el usuario cuando haya pasado las validaciones.
   var finalMsg = '';
 
-  for(var i =0; i < msg.length; i++) {
+  //Iterar el string otorgado por el usuario por medio de un ciclo for.
+  for(var i = 0; i < msg.length; i++){
     var iLetter = msg[i];
-
-    if (iLetter === '0'||
-        iLetter === '1'||
-        iLetter === '2'||
-        iLetter === '3'||
-        iLetter === '4'||
-        iLetter === '5'||
-        iLetter === '6'||
-        iLetter === '7'||
-        iLetter === '8'||
+    // verificar si el valor tiene numeros.
+      if(iLetter === '0' ||
+        iLetter === '1' ||
+        iLetter === '2' ||
+        iLetter === '3' ||
+        iLetter === '4' ||
+        iLetter === '5' ||
+        iLetter === '6' ||
+        iLetter === '7' ||
+        iLetter === '8' ||
         iLetter === '9'){
 
-      alert('La palabra ingresada contiene números, favor de ingresar una palabra para encriptar');
-      return;
+          alert('La palabra ingresada contiene números, favor de ingresar una palabra para encriptar');
+          return;
+    }//cierre de if numeros
+    //creando una variable que almacene el codigo ASCII de la variable msg.
+    var asciiCharCode = msg.charCodeAt(i);
+    //creando variable que almacene los espacios a recorrer.
+    var totalOfSpaces = 33;
+    //creando variable con la fórmula del codigo César.
+    var newCharCode = ((asciiCharCode - 65 + totalOfSpaces) % 26) + 65;
+      finalMsg = finalMsg + String.fromCharCode(newCharCode);
+
+    }
+      return finalMsg;
+
     }
 
-    var asciiCharCode = msg.charCodeAt(i);
-    var totalOfSpaces = 33;
-    var newCharCode = ((asciiCharCode - 65 - totalOfSpaces) % 26) + 65;
-    finalMsg = finalMsg + String.fromCharCode(newCharCode);
-
 }
-
-  return finalMsg;
-
-   }
-
-}// cierre de función decipher
 
 alert('La palabra que ingresaste convertida a Cifrado de César es: ' + cipher());
 console.log(cipher());
