114d7d60749b04f09d25f79a4474a9c6f2470dd4
KarlaMartinez17 <karla.martinez_@hotmail.com>
2018-04-11 13:08:11 -0500

    proyecto final con correcciones en indentacion, y errores de codigo

--- JS/app.js
+++ JS/app.js
@@ -53,9 +53,9 @@ function decipher(){
   if(msg.length === 0){
     alert('Por favor ingresa una palabra para encriptar');
     return;
- }// cierre de If
+  }// cierre de If
 
-msg = msg.toUpperCase();
+  msg = msg.toUpperCase();
 
   //creando una variable que almacene el valor otorgado por el usuario cuando haya pasado las validaciones.
   var finalMsg = '';
@@ -63,7 +63,7 @@ msg = msg.toUpperCase();
   //Iterar el string otorgado por el usuario por medio de un ciclo for.
   for(var i = 0; i < msg.length; i++){
     var iLetter = msg[i];
-    // verificar si el valor tiene numeros.
+  // verificar si el valor tiene numeros.
       if(iLetter === '0' ||
         iLetter === '1' ||
         iLetter === '2' ||
@@ -77,19 +77,18 @@ msg = msg.toUpperCase();
 
           alert('La palabra ingresada contiene números, favor de ingresar una palabra para encriptar');
           return;
-    }//cierre de if numeros
-    //creando una variable que almacene el codigo ASCII de la variable msg.
-    var asciiCharCode = msg.charCodeAt(i);
-    //creando variable que almacene los espacios a recorrer.
-    var totalOfSpaces = 33;
-    //creando variable con la fórmula del codigo César.
-    var newCharCode = ((asciiCharCode - 65 + totalOfSpaces) % 26) + 65;
+  }//cierre de if numeros
+  //creando una variable que almacene el codigo ASCII de la variable msg.
+  var asciiCharCode = msg.charCodeAt(i);
+  //creando variable que almacene los espacios a recorrer.
+  var totalOfSpaces = 33;
+  //creando variable con la fórmula del codigo César.
+  var newCharCode = ((asciiCharCode - 65 - totalOfSpaces) % 26) + 65;
       finalMsg = finalMsg + String.fromCharCode(newCharCode);
 
-    }
+  }
       return finalMsg;
-
-    }
+}
 
 
 
